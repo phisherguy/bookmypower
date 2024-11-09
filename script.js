886f3af1072dc52691b6fa3181c05585268fce9c
phisherguy <phisherguyfb@gmail.com>
2024-11-09 16:40:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,959 @@
+const solarPrices = [
+  96589, 145379, 192598, 244183, 302825, 346934, 408708, 450404, 498957, 539331,
+];
+const indiaStatesAndDistricts = {
+  "Andaman and Nicobar Islands": [
+    "Nicobar",
+    "North and Middle Andaman",
+    "South Andaman",
+  ],
+  "Andhra Pradesh": [
+    "Anantapur",
+    "Chittoor",
+    "East Godavari",
+    "Guntur",
+    "Kadapa",
+    "Krishna",
+    "Kurnool",
+    "Nellore",
+    "Prakasam",
+    "Srikakulam",
+    "Visakhapatnam",
+    "Vizianagaram",
+    "West Godavari",
+    "Kadapa",
+  ],
+  "Arunachal Pradesh": [
+    "Anjaw",
+    "Changlang",
+    "Dibang Valley",
+    "East Kameng",
+    "East Siang",
+    "Kamle",
+    "Kra Daadi",
+    "Kurung Kumey",
+    "Lepa Rada",
+    "Lohit",
+    "Longding",
+    "Lower Dibang Valley",
+    "Lower Siang",
+    "Lower Suba",
+    "Namsai",
+    "Pakke Kessang",
+    "Papum Pare",
+    "Shi Yomi",
+    "Siang",
+    "Tawang",
+    "Tirap",
+    "Upper Siang",
+    "Upper Subansiri",
+    "West Kameng",
+    "West Siang",
+  ],
+  Assam: [
+    "Baksa",
+    "Barpeta",
+    "Biswanath",
+    "Bongaigaon",
+    "Cachar",
+    "Charaideo",
+    "Chirang",
+    "Darrang",
+    "Dhemaji",
+    "Dhubri",
+    "Dibrugarh",
+    "Dima Hasao",
+    "Goalpara",
+    "Golaghat",
+    "Hailakandi",
+    "Hojai",
+    "Jorhat",
+    "Kamrup Metropolitan",
+    "Kamrup Rural",
+    "Karbi Anglong",
+    "Karimganj",
+    "Kokrajhar",
+    "Lakhimpur",
+    "Majuli",
+    "Morigaon",
+    "Nagaon",
+    "Nalbari",
+    "Sivasagar",
+    "Sonitpur",
+    "South Salmara Mankachar",
+    "Tinsukia",
+    "Udalguri",
+    "West Karbi Anglong",
+  ],
+  Bihar: [
+    "Araria",
+    "Arwal",
+    "Aurangabad",
+    "Banka",
+    "Begusarai",
+    "Bhagalpur",
+    "Bhojpur",
+    "Buxar",
+    "Darbhanga",
+    "East Champaran",
+    "Gaya",
+    "Gopalganj",
+    "Jamui",
+    "Jehanabad",
+    "Kaimur",
+    "Katihar",
+    "Khagaria",
+    "Kishanganj",
+    "Lakhisarai",
+    "Madhepura",
+    "Madhubani",
+    "Munger",
+    "Muzaffarpur",
+    "Nalanda",
+    "Nawada",
+    "Patna",
+    "Purnia",
+    "Rohtas",
+    "Saharsa",
+    "Samastipur",
+    "Saran",
+    "Sheikhpura",
+    "Sheohar",
+    "Sitamarhi",
+    "Siwan",
+    "Supaul",
+    "Vaishali",
+    "West Champaran",
+  ],
+  Chhattisgarh: [
+    "Balod",
+    "Baloda Bazar",
+    "Balrampur",
+    "Bastar",
+    "Bemetara",
+    "Bijapur",
+    "Bilaspur",
+    "Dantewada",
+    "Dhamtari",
+    "Durg",
+    "Gariaband",
+    "Janjgir Champa",
+    "Jashpur",
+    "Kanker",
+    "Kawardha",
+    "Kondagaon",
+    "Korba",
+    "Korea",
+    "Mahasamund",
+    "Mungeli",
+    "Narayanpur",
+    "Raigarh",
+    "Raipur",
+    "Rajnandgaon",
+    "Sukma",
+    "Surajpur",
+    "Surguja",
+  ],
+  Goa: ["North Goa", "South Goa"],
+  Gujarat: [
+    "Ahmedabad",
+    "Amreli",
+    "Anand",
+    "Aravalli",
+    "Banaskantha",
+    "Bharuch",
+    "Bhavnagar",
+    "Botad",
+    "Chhota Udaipur",
+    "Dahod",
+    "Dang",
+    "Devbhoomi Dwarka",
+    "Gandhinagar",
+    "Gir Somnath",
+    "Jamnagar",
+    "Junagadh",
+    "Kheda",
+    "Kutch",
+    "Mahisagar",
+    "Mehsana",
+    "Morbi",
+    "Narmada",
+    "Navsari",
+    "Panchmahal",
+    "Patan",
+    "Porbandar",
+    "Rajkot",
+    "Sabarkantha",
+    "Surat",
+    "Surendranagar",
+    "Tapi",
+    "Vadodara",
+    "Valsad",
+  ],
+  Haryana: [
+    "Ambala",
+    "Bhiwani",
+    "Charkhi Dadri",
+    "Faridabad",
+    "Fatehabad",
+    "Gurugram",
+    "Hisar",
+    "Jhajjar",
+    "Jind",
+    "Kaithal",
+    "Karnal",
+    "Kurukshetra",
+    "Mahendragarh",
+    "Nuh",
+    "Palwal",
+    "Panchkula",
+    "Panipat",
+    "Rewari",
+    "Rohtak",
+    "Sirsa",
+    "Sonipat",
+    "Yamunanagar",
+  ],
+  "Himachal Pradesh": [
+    "Bilaspur",
+    "Chamba",
+    "Hamirpur",
+    "Kangra",
+    "Kinnaur",
+    "Kullu",
+    "Lahaul and Spiti",
+    "Mandi",
+    "Shimla",
+    "Sirmaur",
+    "Solan",
+    "Una",
+  ],
+  Jharkhand: [
+    "Bokaro",
+    "Chatra",
+    "Deoghar",
+    "Dhanbad",
+    "Dumka",
+    "East Singhbhum",
+    "Garhwa",
+    "Giridih",
+    "Hazaribagh",
+    "Jamtara",
+    "Khunti",
+    "Koderma",
+    "Latehar",
+    "Lohardaga",
+    "Pakur",
+    "Palamu",
+    "Ramgarh",
+    "Ranchi",
+    "Sahibganj",
+    "Seraikela Kharsawan",
+    "Simdega",
+    "West Singhbhum",
+  ],
+  Karnataka: [
+    "Bagalkot",
+    "Ballari",
+    "Belagavi",
+    "Bengaluru Rural",
+    "Bengaluru Urban",
+    "Bidar",
+    "Chamarajanagar",
+    "Chikballapur",
+    "Chikkamagaluru",
+    "Chitradurga",
+    "Dakshina Kannada",
+    "Davangere",
+    "Dharwad",
+    "Gadag",
+    "Hassan",
+    "Haveri",
+    "Kalaburagi",
+    "Kodagu",
+    "Kolar",
+    "Koppal",
+    "Mandya",
+    "Mysuru",
+    "Raichur",
+    "Ramanagara",
+    "Shivamogga",
+    "Tumakuru",
+    "Udupi",
+    "Uttara Kannada",
+    "Vijayapura",
+    "Yadgir",
+  ],
+  Kerala: [
+    "Alappuzha",
+    "Ernakulam",
+    "Idukki",
+    "Kannur",
+    "Kasaragod",
+    "Kollam",
+    "Kottayam",
+    "Kozhikode",
+    "Malappuram",
+    "Palakkad",
+    "Pathanamthitta",
+    "Thiruvananthapuram",
+    "Thrissur",
+    "Wayanad",
+  ],
+  "Madhya Pradesh": [
+    "Agar Malwa",
+    "Alirajpur",
+    "Anuppur",
+    "Ashoknagar",
+    "Balaghat",
+    "Barwani",
+    "Betul",
+    "Bhind",
+    "Bhopal",
+    "Burhanpur",
+    "Chhatarpur",
+    "Chhindwara",
+    "Damoh",
+    "Datia",
+    "Dewas",
+    "Dhar",
+    "Dindori",
+    "Guna",
+    "Gwalior",
+    "Harda",
+    "Hoshangabad",
+    "Indore",
+    "Jabalpur",
+    "Jhabua",
+    "Katni",
+    "Khandwa",
+    "Khargone",
+    "Mandla",
+    "Mandsaur",
+    "Morena",
+    "Narsinghpur",
+    "Neemuch",
+    "Panna",
+    "Raisen",
+    "Rajgarh",
+    "Ratlam",
+    "Rewa",
+    "Sagar",
+    "Satna",
+    "Sehore",
+    "Seoni",
+    "Shahdol",
+    "Shajapur",
+    "Sheopur",
+    "Shivpuri",
+    "Sidhi",
+    "Singrauli",
+    "Tikamgarh",
+    "Ujjain",
+    "Umaria",
+    "Vidisha",
+  ],
+  Maharashtra: [
+    "Ahmednagar",
+    "Akola",
+    "Amravati",
+    "Aurangabad",
+    "Beed",
+    "Bhandara",
+    "Buldhana",
+    "Chandrapur",
+    "Dhule",
+    "Gadchiroli",
+    "Gondia",
+    "Hingoli",
+    "Jalgaon",
+    "Jalna",
+    "Kolhapur",
+    "Latur",
+    "Mumbai City",
+    "Mumbai Suburban",
+    "Nagpur",
+    "Nanded",
+    "Nandurbar",
+    "Nashik",
+    "Osmanabad",
+    "Palghar",
+    "Parbhani",
+    "Pune",
+    "Raigad",
+    "Ratnagiri",
+    "Sangli",
+    "Satara",
+    "Sindhudurg",
+    "Solapur",
+    "Thane",
+    "Wardha",
+    "Washim",
+    "Yavatmal",
+  ],
+  Manipur: [
+    "Bishnupur",
+    "Chandel",
+    "Churachandpur",
+    "Imphal East",
+    "Imphal West",
+    "Jiribam",
+    "Kakching",
+    "Kamjong",
+    "Kangpokpi",
+    "Noney",
+    "Pherzawl",
+    "Senapati",
+    "Tamenglong",
+    "Tengnoupal",
+    "Thoubal",
+    "Ukhrul",
+  ],
+  Meghalaya: [
+    "East Garo Hills",
+    "East Jaintia Hills",
+    "East Khasi Hills",
+    "North Garo Hills",
+    "Ri Bhoi",
+    "South Garo Hills",
+    "South West Garo Hills",
+    "South West Khasi Hills",
+    "West Garo Hills",
+    "West Jaintia Hills",
+    "West Khasi Hills",
+  ],
+  Mizoram: [
+    "Aizawl",
+    "Champhai",
+    "Hnahthial",
+    "Khawzawl",
+    "Kolasib",
+    "Lawngtlai",
+    "Lunglei",
+    "Mamit",
+    "Saiha",
+    "Saitual",
+    "Serchhip",
+  ],
+  Nagaland: [
+    "Dimapur",
+    "Kiphire",
+    "Kohima",
+    "Longleng",
+    "Mokokchung",
+    "Mon",
+    "Peren",
+    "Phek",
+    "Tuensang",
+    "Wokha",
+    "Zunheboto",
+  ],
+  Odisha: [
+    "Angul",
+    "Balangir",
+    "Balasore",
+    "Bargarh",
+    "Bhadrak",
+    "Boudh",
+    "Cuttack",
+    "Deogarh",
+    "Dhenkanal",
+    "Gajapati",
+    "Ganjam",
+    "Jagatsinghpur",
+    "Jajpur",
+    "Jharsuguda",
+    "Kalahandi",
+    "Kandhamal",
+    "Kendrapara",
+    "Kendujhar",
+    "Khordha",
+    "Koraput",
+    "Malkangiri",
+    "Mayurbhanj",
+    "Nabarangpur",
+    "Nayagarh",
+    "Nuapada",
+    "Puri",
+    "Rayagada",
+    "Sambalpur",
+    "Subarnapur",
+    "Sundargarh",
+  ],
+  Puducherry: ["Karaikal", "Mahe", "Puducherry", "Yanam"],
+  Punjab: [
+    "Amritsar",
+    "Barnala",
+    "Bathinda",
+    "Faridkot",
+    "Fatehgarh Sahib",
+    "Fazilka",
+    "Ferozepur",
+    "Gurdaspur",
+    "Hoshiarpur",
+    "Jalandhar",
+    "Kapurthala",
+    "Ludhiana",
+    "Mansa",
+    "Moga",
+    "Muktsar",
+    "Pathankot",
+    "Patiala",
+    "Rupnagar",
+    "Sangrur",
+    "Shaheed Bhagat Singh Nagar",
+    "Sri Muktsar Sahib",
+    "Tarn Taran",
+  ],
+  Rajasthan: [
+    "Ajmer",
+    "Alwar",
+    "Banswara",
+    "Baran",
+    "Barmer",
+    "Bharatpur",
+    "Bhilwara",
+    "Bikaner",
+    "Bundi",
+    "Chittorgarh",
+    "Churu",
+    "Dausa",
+    "Dholpur",
+    "Dungarpur",
+    "Hanumangarh",
+    "Jaipur",
+    "Jaisalmer",
+    "Jalore",
+    "Jhalawar",
+    "Jhunjhunu",
+    "Jodhpur",
+    "Karauli",
+    "Kota",
+    "Nagaur",
+    "Pali",
+    "Pratapgarh",
+    "Rajsamand",
+    "Sawai Madhopur",
+    "Sikar",
+    "Sirohi",
+    "Sri Ganganagar",
+    "Tonk",
+    "Udaipur",
+  ],
+  Sikkim: ["East Sikkim", "North Sikkim", "South Sikkim", "West Sikkim"],
+  "Tamil Nadu": [
+    "Ariyalur",
+    "Chengalpattu",
+    "Chennai",
+    "Coimbatore",
+    "Cuddalore",
+    "Dharmapuri",
+    "Dindigul",
+    "Erode",
+    "Kallakurichi",
+    "Kanchipuram",
+    "Kanyakumari",
+    "Karur",
+    "Krishnagiri",
+    "Madurai",
+    "Mayiladuthurai",
+    "Nagapattinam",
+    "Namakkal",
+    "Nilgiris",
+    "Perambalur",
+    "Pudukkottai",
+    "Ramanathapuram",
+    "Ranipet",
+    "Salem",
+    "Sivaganga",
+    "Tenkasi",
+    "Thanjavur",
+    "Theni",
+    "Thoothukudi",
+    "Tiruchirappalli",
+    "Tirunelveli",
+    "Tirupathur",
+    "Tiruppur",
+    "Tiruvallur",
+    "Tiruvannamalai",
+    "Tiruvarur",
+    "Vellore",
+    "Viluppuram",
+    "Virudhunagar",
+  ],
+  Telangana: [
+    "Adilabad",
+    "Bhadradri Kothagudem",
+    "Hyderabad",
+    "Jagtial",
+    "Jangaon",
+    "Jayashankar Bhupalpally",
+    "Jogulamba Gadwal",
+    "Kamareddy",
+    "Karimnagar",
+    "Khammam",
+    "Komaram Bheem Asjad",
+    "Mahabubabad",
+    "Mahabubnagar",
+    "Mancherial",
+    "Medak",
+    "Medchal Malkajgiri",
+    "Mulugu",
+    "Nagarkurnool",
+    "Nalgonda",
+    "Narayanpet",
+    "Nirmal",
+    "Nizamabad",
+    "Peddapalli",
+    "Rajanna Sircilla",
+    "Rangareddy",
+    "Sangareddy",
+    "Siddipet",
+    "Suryapet",
+    "Vikarabad",
+    "Wanaparthy",
+    "Warangal Rural",
+    "Warangal Urban",
+    "Yadadri Bhuvanagiri",
+  ],
+  Tripura: [
+    "Dhalai",
+    "Gomati",
+    "Khowai",
+    "North Tripura",
+    "Sepahijala",
+    "South Tripura",
+    "Unakoti",
+    "West Tripura",
+  ],
+  "Uttar Pradesh": [
+    "Agra",
+    "Aligarh",
+    "Ambedkar Nagar",
+    "Amethi",
+    "Amroha",
+    "Auraiya",
+    "Ayodhya",
+    "Azamgarh",
+    "Baghpat",
+    "Bahraich",
+    "Ballia",
+    "Balrampur",
+    "Banda",
+    "Barabanki",
+    "Bareilly",
+    "Basti",
+    "Bhadohi",
+    "Bijnor",
+    "Budaun",
+    "Bulandshahr",
+    "Chandauli",
+    "Chitrakoot",
+    "Deoria",
+    "Etah",
+    "Etawah",
+    "Farrukhabad",
+    "Fatehpur",
+    "Firozabad",
+    "Gautam Buddha Nagar",
+    "Ghaziabad",
+    "Ghazipur",
+    "Gonda",
+    "Gorakhpur",
+    "Hamirpur",
+    "Hapur",
+    "Hardoi",
+    "Hathras",
+    "Jalaun",
+    "Jaunpur",
+    "Jhansi",
+    "Kannauj",
+    "Kanpur Dehat",
+    "Kanpur Nagar",
+    "Kasganj",
+    "Kaushambi",
+    "Kushinagar",
+    "Lakhimpur Kheri",
+    "Lalitpur",
+    "Lucknow",
+    "Maharajganj",
+    "Mahoba",
+    "Mainpuri",
+    "Mathura",
+    "Mau",
+    "Meerut",
+    "Mirzapur",
+    "Moradabad",
+    "Muzaffarnagar",
+    "Pilibhit",
+    "Pratapgarh",
+    "Prayagraj",
+    "Raebareli",
+    "Rampur",
+    "Saharanpur",
+    "Sambhal",
+    "Sant Kabir Nagar",
+    "Shahjahanpur",
+    "Shamli",
+    "Shrawasti",
+    "Siddharthnagar",
+    "Sitapur",
+    "Sonbhadra",
+    "Sultanpur",
+    "Unnao",
+    "Varanasi",
+  ],
+  Uttarakhand: [
+    "Almora",
+    "Bageshwar",
+    "Chamoli",
+    "Champawat",
+    "Dehradun",
+    "Haridwar",
+    "Nainital",
+    "Pauri Garhwal",
+    "Pithoragarh",
+    "Rudraprayag",
+    "Tehri Garhwal",
+    "Udham Singh Nagar",
+    "Uttarkashi",
+  ],
+  "West Bengal": [
+    "Alipurduar",
+    "Bankura",
+    "Birbhum",
+    "Cooch Behar",
+    "Dakshin Dinajpur",
+    "Darjeeling",
+    "Hooghly",
+    "Howrah",
+    "Jalpaiguri",
+    "Jhargram",
+    "Kalimpong",
+    "Kolkata",
+    "Malda",
+    "Murshidabad",
+    "Nadia",
+    "North 24 Parganas",
+    "Paschim Bardhaman",
+    "Paschim Medinipur",
+    "Purba Bardhaman",
+    "Purba Medinipur",
+    "Purulia",
+    "South 24 Parganas",
+    "Uttar Dinajpur",
+  ],
+};
+
+window.onload = function () {
+  document.getElementById("myInput").value = 0;
+  loadValues();
+  populateStates();
+};
+
+function loadValues() {
+  // fillStateDD();
+  let solarType = document.getElementById("solarTypeId").value;
+  const selectedbillorkw = document.querySelector(
+    'input[name="billorkw"]:checked'
+  ).value;
+  const inputField = document.getElementById("myInput");
+  if (selectedbillorkw == "bill") {
+    inputField.placeholder = "Enter Bill Amount";
+  } else {
+    inputField.placeholder = "Enter System Size";
+  }
+  performCalculation(solarType, selectedbillorkw, inputField.value);
+}
+
+function performCalculation(solarType, selectedbillorkw, value) {
+  if (!value || value < 1) return;
+  let result = {
+    system: 0,
+    space: 0,
+    energy: 0,
+    savings: 0,
+    price: 0,
+    subsidy: 0,
+  };
+  if (solarType == "home") {
+    if (selectedbillorkw == "bill") {
+      result.system = parseInt(value) / 7 / (4 * 30);
+      result.savings = parseInt(value) || 0;
+    } else if (selectedbillorkw == "kw") {
+      result.system = parseFloat(value);
+      result.savings = result.system * 120 * 7;
+    }
+
+    result.space = result.system * 80;
+    result.energy = result.system * 120 * 12;
+    result.system = Math.round(result.system);
+    result.energy = Math.round(result.energy);
+    result.space = Math.round(result.space) || 0;
+    result.savings = Math.round(result.savings) * 12 || 0;
+    result.price = solarPrices[result.system - 1] || 0;
+
+    if (result.system === 1) {
+      result.subsidy = 30000;
+    } else if (result.system === 2) {
+      result.subsidy = 60000;
+    } else if (result.system >= 3 && result.system <= 10) {
+      result.subsidy = 78000;
+    }
+  } else {
+    if (selectedbillorkw == "bill") {
+      result.system = parseInt(value) / 9 / (4 * 30);
+      result.savings = parseInt(value) || 0;
+    } else if (selectedbillorkw == "kw") {
+      result.system = parseFloat(value);
+      result.savings = result.system * 120 * 9;
+    }
+
+    result.space = result.system * 80;
+    result.energy = result.system * 120 * 12;
+    result.system = Math.round(result.system);
+    result.energy = Math.round(result.energy);
+    result.space = Math.round(result.space) || 0;
+    result.savings = Math.round(result.savings) * 12 || 0;
+    result.price = solarPrices[result.system - 1] || 0;
+  }
+
+  setOutPut(result);
+}
+
+function setOutPut(result) {
+  document.getElementById("systemOP").innerText = result.system ?? 0;
+  document.getElementById("spaceOP").innerText = result.space ?? 0;
+  document.getElementById("energyOP").innerText = result.energy ?? 0;
+  document.getElementById("savingsOP").innerText =
+    result.savings.toLocaleString("en-IN") ?? 0;
+  document.getElementById("priceOP").innerText =
+    result.price.toLocaleString("en-IN") ?? 0;
+  document.getElementById("subsisdyOP").innerText =
+    result.subsidy.toLocaleString("en-IN") ?? 0;
+}
+
+// MODAL
+
+const contactBtn = document.getElementById("contactBtn");
+const contactModal = document.getElementById("contactModal");
+const closeBtn = document.querySelector(".close-btn");
+const contactForm = document.getElementById("contactForm");
+
+contactBtn.addEventListener("click", () => {
+  contactModal.style.display = "flex"; // Show modal
+});
+
+closeBtn.addEventListener("click", () => {
+  contactModal.style.display = "none"; // Close modal
+});
+
+// Close the modal if the user clicks anywhere outside of it
+window.addEventListener("click", (event) => {
+  if (event.target === contactModal) {
+    contactModal.style.display = "none";
+  }
+});
+
+// Form Submission
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent the form from submitting
+
+  let data = {
+    fullName: document.getElementById("fullName").value,
+    phone: document.getElementById("phone").value,
+    email: document.getElementById("email").value,
+    state: document.getElementById("stateDD").value,
+    district: document.getElementById("districtDropdown").value,
+    solarFor: document.getElementById("solarForDD").value,
+    avgBill: document.getElementById("avgBillDD").value,
+    finance: document.getElementById("financeDD").value,
+  };
+  UpdateData(data);
+  // Close modal after form submission
+  contactModal.style.display = "none";
+});
+
+// Function to populate state dropdown
+function populateStates() {
+  const stateDropdown = document.getElementById("stateDD");
+  // Clear any existing options
+  stateDropdown.innerHTML = "<option value=''>Select State</option>";
+
+  // Populate states
+  Object.keys(indiaStatesAndDistricts).forEach((state) => {
+    const option = document.createElement("option");
+    option.value = state;
+    option.textContent = state;
+    stateDropdown.appendChild(option);
+  });
+}
+
+// Function to populate district dropdown based on selected state
+function populateDistricts() {
+  const stateDropdown = document.getElementById("stateDD");
+  const districtDropdown = document.getElementById("districtDropdown");
+
+  // Get selected state
+  const selectedState = stateDropdown.value;
+
+  // Clear previous district options
+  districtDropdown.innerHTML = "<option value=''>Select District</option>";
+
+  // If a valid state is selected
+  if (selectedState && indiaStatesAndDistricts[selectedState]) {
+    // Show district dropdown
+    districtDropdown.style.display = "block";
+
+    // Populate districts for the selected state
+    indiaStatesAndDistricts[selectedState].forEach((district) => {
+      const option = document.createElement("option");
+      option.value = district;
+      option.textContent = district;
+      districtDropdown.appendChild(option);
+    });
+  } else {
+    // Hide district dropdown if no state is selected
+    districtDropdown.style.display = "none";
+  }
+}
+
+function UpdateData(data) {
+  const currentDateTime = new Date().toLocaleString();
+  data.timestamp = currentDateTime;
+  const sheetUpdater =
+    "https://us-central1-demoapp-d8d36.cloudfunctions.net/updateGoogleSheet";
+  const requestData = {
+    spreadsheetId: "1I03-UL1FKJlzrUAeksNxIfzfN-O_CZ03bbfI_kIXnwo",
+    range: "Sheet1!A2",
+    values: [
+      [
+        data.fullName,
+        data.phone,
+        data.email,
+        data.state,
+        data.district,
+        data.solarFor,
+        data.avgBill,
+        data.finance,
+        data.timestamp,
+      ],
+    ],
+  };
+  fetch(sheetUpdater, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      alert("Thank you for your inquiry. We will get back to you soon!");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Failed to send enquiry");
+    });
+}
